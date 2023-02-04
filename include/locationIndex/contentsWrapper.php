4448b7f2df503c1d7436c970829f6ff1071f10bd
DESKTOP-A4FQCPF\sangg <sanggusbab@gmail.com>
2023-02-04 20:27:31 +0900

    locationIndex updated

--- include/locationIndex/contentsWrapper.php
+++ include/locationIndex/contentsWrapper.php
@@ -4,17 +4,20 @@
     <br>
 </P>
 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3161.832511015934!2d127.02408121531202!3d37.58256147979495!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357cbd4279f29c71%3A0xe4e68bbe651c1494!2z6rOg66Ck64yA7ZWZ6rWQIOyLoOqzte2Vmeq0gA!5e0!3m2!1sko!2skr!4v1675509020385!5m2!1sko!2skr" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+
+<br>
+<br>
 <br>
 <b style="font-size: 24px;">상세 정보</b>
 <br>
 <p style="float: left;">
     <img src="./images/resource/mapPinImage.jpg" alt="mapPinImage" style="width: 15px;">
-    <span style="font-size: 15px">
+    <span style="font-size: 12px; color: #bbb;">
         서울특별시 성북구 안암로 145 신공학관 109호 <br>
         Room 109, New Engineering Building, 145, Anam-ro, Seongbuk-gu, Seoul, Republic of Korea
     </span>
 </p>
-<p>
+<p style="font-size: 12px; color: #bbb;">
     지번 : 안암동 5가 126-16 신공학관 109호
 </p>
 <br>
