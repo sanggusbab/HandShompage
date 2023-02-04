cd310a6cab10e9f7a69b89e3a96adcfd30cdcb05
DESKTOP-A4FQCPF\sangg <sanggusbab@gmail.com>
2023-02-04 22:09:52 +0900

    something

--- connect/connect.php
+++ connect/connect.php
@@ -1,12 +1,11 @@
 <?php
-    $host = "localhost";
-    $user = "default";
-    $password = "@elvhfxmrlqhstkdydwk!23";
-    $dbName = "handsdb";
+$host = "localhost";
+$user = "default";
+$password = "@elvhfxmrlqhstkdydwk!23";
+$dbName = "handsdb";
 
-    if($this->dbConnection == null)
-    {
-        $this->dbConnection = new mysqli($host, $user, $password, $dbName);
-        $this->dbConnection->set_charset("utf8");
-    }
+if ($this->dbConnection == null) {
+    $this->dbConnection = new mysqli($host, $user, $password, $dbName);
+    $this->dbConnection->set_charset("utf8");
+}
 ?>
--- database/handsdb.php
+++ database/handsdb.php
@@ -1,21 +1,23 @@
 <?php
-    class handsdb{
-        // 데이터베이스 접속 정보를 대입할 프로퍼티
-        protected $dbConnection = null;
+class handsdb
+{
+    // 데이터베이스 접속 정보를 대입할 프로퍼티
+    protected $dbConnection = null;
 
-        //mode
-        protected $mode;
-        //데이터베이스 접속 정보를 가져오는 메소드
-        protected function dbConnection(){
-            include_once $_SERVER['DOCUMENT_ROOT'].'/connect/connect.php';
-        }
+    //mode
+    protected $mode;
+    //데이터베이스 접속 정보를 가져오는 메소드
+    protected function dbConnection()
+    {
+        include_once $_SERVER['DOCUMENT_ROOT'] . '/connect/connect.php';
+    }
 
-        //생성자
-        function __construct(){
-            //mode값에 따라 메소드 호출
-            if(isset($_POST['mode'])){
-
-            }
+    //생성자
+    function __construct()
+    {
+        //mode값에 따라 메소드 호출
+        if (isset($_POST['mode'])) {
         }
     }
+}
 ?>
