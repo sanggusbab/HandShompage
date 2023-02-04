cbcfe0f7d9eef1b0074f9982cd2f1d0b2ff8346e
DESKTOP-A4FQCPF\sangg <sanggusbab@gmail.com>
2023-02-04 20:18:44 +0900

    image sample uploaded

--- include/locationIndex/contentsWrapper.php
+++ include/locationIndex/contentsWrapper.php
@@ -3,4 +3,23 @@
     <br>
     <br>
 </P>
-<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3161.832511015934!2d127.02408121531202!3d37.58256147979495!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357cbd4279f29c71%3A0xe4e68bbe651c1494!2z6rOg66Ck64yA7ZWZ6rWQIOyLoOqzte2Vmeq0gA!5e0!3m2!1sko!2skr!4v1675509020385!5m2!1sko!2skr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3161.832511015934!2d127.02408121531202!3d37.58256147979495!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357cbd4279f29c71%3A0xe4e68bbe651c1494!2z6rOg66Ck64yA7ZWZ6rWQIOyLoOqzte2Vmeq0gA!5e0!3m2!1sko!2skr!4v1675509020385!5m2!1sko!2skr" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+<br>
+<b style="font-size: 24px;">상세 정보</b>
+<br>
+<p>
+    <span>
+        <img src="./images/resouece/mapPinImage.jpg" alt="mapPinImage">
+    </span>
+    <span>
+        서울특별시 성북구 안암로 145 신공학관 109호 | 145, Anam-ro, Seongbuk-gu, Seoul, Republic of Korea
+    </span>
+</p>
+<p>
+    지번 : 안암동 5가 126-16 신공학관 109호
+</p>
+<br>
+<p>
+    <span>궁금하신 내용이 있으시다면? </span>
+    <span><a href="./contactusIndex.php">문의하기(CONTACT US)클릭</a></span>
+</p>
