ea823c7f5d37bd2d2c9226ecc426e535cb903def
DESKTOP-A4FQCPF\sangg <sanggusbab@gmail.com>
2023-02-04 18:37:20 +0900

    About contents completed

--- include/historyIndex/contentsWrapper.php
+++ include/historyIndex/contentsWrapper.php
@@ -14,7 +14,7 @@
                 1985 전자공학과 전자전시회 주관<br>
                 1990 제 1회 HandS 전시회 개최<br>
                 2016년 HandS 30주년 행사<br>
-                2022 HandS 홈커밍 행사<br>
+                2022 HandS 홈커밍 행사
             </p>
         </div>
     </div>
@@ -27,35 +27,30 @@
                 2019 임베디드 SW경진대회 장려상<br>
                 2020 자율주행모형차 경진대회 대상<br>
                 2020 스마트디바이스 공모전 장려상<br>
-                2021 임베디드 SW경진대회 특선<br>
+                2021 임베디드 SW경진대회 특선
             </p>
         </div>
     </div>
     <div class="HistoryWrapper">
         <img src="images/etc/1st.svg" alt="When Started" class="HistoryImage">
         <div>
-            <p><b>언제 시작했나요?</b></p> <br>
+            <p><b>장소</b></p> <br>
             <p>
-                1981 핸즈 스터디그룹 조직<br>
-                1985 핸즈 조직<br>
-                1985 전자공학과 전자전시회 주관<br>
-                1990 제 1회 HandS 전시회 개최<br>
-                2016년 HandS 30주년 행사<br>
-                2022 HandS 홈커밍 행사<br>
+                신공학관 109호
             </p>
         </div>
     </div>
     <div class="HistoryWrapper">
         <img src="images/etc/1st.svg" alt="When Started" class="HistoryImage">
         <div>
-            <p><b>언제 시작했나요?</b></p> <br>
+            <p><b>주요 활동분야</b></p> <br>
             <p>
-                1981 핸즈 스터디그룹 조직<br>
-                1985 핸즈 조직<br>
-                1985 전자공학과 전자전시회 주관<br>
-                1990 제 1회 HandS 전시회 개최<br>
-                2016년 HandS 30주년 행사<br>
-                2022 HandS 홈커밍 행사<br>
+                자율주행<br>
+                로봇<br>
+                3D 프린터<br>
+                인공지능<br>
+                안드로이드<br>
+                자유주제
             </p>
         </div>
     </div>
