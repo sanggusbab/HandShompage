c2cd09732c1e29f30cd5f8058aa607ff21ea8563
DESKTOP-A4FQCPF\sangg <sanggusbab@gmail.com>
2023-02-04 20:02:12 +0900

    image sample uploaded

--- include/locationIndex/contentsWrapper.php
+++ include/locationIndex/contentsWrapper.php
@@ -6,20 +6,20 @@
 <div class="rows" style="text-align: center;">
     <!-- * 카카오맵 - 지도퍼가기 -->
     <!-- 1. 지도 노드 -->
-    <div id="daumRoughmapContainer1675507210527" class="root_daum_roughmap root_daum_roughmap_landing"></div>
-    
+    <div id="daumRoughmapContainer1675508515386" class="root_daum_roughmap root_daum_roughmap_landing"></div>
+
     <!--
-       2. 설치 스크립트
-       * 지도 퍼가기 서비스를 2개 이상 넣을 경우, 설치 스크립트는 하나만 삽입합니다.
-    -->
+	2. 설치 스크립트
+	* 지도 퍼가기 서비스를 2개 이상 넣을 경우, 설치 스크립트는 하나만 삽입합니다.
+-->
     <script charset="UTF-8" class="daum_roughmap_loader_script" src="https://ssl.daumcdn.net/dmaps/map_js_init/roughmapLoader.js"></script>
-    
+
     <!-- 3. 실행 스크립트 -->
     <script charset="UTF-8">
         new daum.roughmap.Lander({
-            "timestamp": "1675507210527",
-            "key": "2dn8n",
-            "mapWidth": "1280",
+            "timestamp": "1675508515386",
+            "key": "2dn8o",
+            "mapWidth": "640",
             "mapHeight": "360"
         }).render();
     </script>
