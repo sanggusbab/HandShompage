35082436e41191ae86c7709fac3ba6dada27190c
DESKTOP-A4FQCPF\sangg <sanggusbab@gmail.com>
2023-02-06 04:10:05 +0900

    font awesome all downloaded

--- contactusIndex.php
+++ contactusIndex.php
@@ -16,7 +16,22 @@
     <link rel="stylesheet" href="./css/sideBarFooter.css">
     <link rel="stylesheet" href="./cssIndexes/contentsContainer.css">
     <link rel="stylesheet" href="./cssIndexes/contactusIndex/contentsWrapper.css">
-    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- update the core fontawesome file -->
+    <link href="/fontawesome-free-6.2.1-web/css/fontawesome.css" rel="stylesheet">
+    <!-- update any styles .css files you have linked -->
+    <link href="/fontawesome-free-6.2.1-web/css/solid.css" rel="stylesheet">
+    <link href="/fontawesome-free-6.2.1-web/css/brands.css" rel="stylesheet">
+    <!-- update existing v5 CSS to use v6 icons and assets -->
+    <link href="/fontawesome-free-6.2.1-web/css/v5-font-face.css" rel="stylesheet" />
+    <!-- update the all.css file -->
+    <link href="/fontawesome-free-6.2.1-web/css/all.css" rel="stylesheet">
+    <!-- update any styles .js files you have linked -->
+    <script defer src="/fontawesome-free-6.2.1-web/js/solid.js"></script>
+    <script defer src="/fontawesome-free-6.2.1-web/js/brands.js"></script>
+    <!-- update the core fontawesome.js file -->
+    <script defer src="/fontawesome-free-6.2.1-web/js/fontawesome.js"></script>
+    <!-- update the all.js file -->
+    <script defer src="/fontawesome-free-6.2.1-web/js/all.js"></script>
     <script type="text/javascript" src="./js/jquery-3.1.0.min.js"></script>
 </head>
 
--- index.php
+++ index.php
@@ -15,10 +15,24 @@
     <link rel="stylesheet" href="./css/footer.css">
     <link rel="stylesheet" href="./css/header.css">
     <link rel="stylesheet" href="./css/sideBarFooter.css">
-    <link rel="stylesheet" type="text/css" href="https://use.fontawesome.com/releases/v5.0.12/css/all.css"
-    >
-    <script type="text/javascript" src="./js/jquery-3.1.0.min.js"></script>
+    <!-- update the core fontawesome file -->
+    <link href="/fontawesome-free-6.2.1-web/css/fontawesome.css" rel="stylesheet">
+    <!-- update any styles .css files you have linked -->
+    <link href="/fontawesome-free-6.2.1-web/css/solid.css" rel="stylesheet">
+    <link href="/fontawesome-free-6.2.1-web/css/brands.css" rel="stylesheet">
+    <!-- update existing v5 CSS to use v6 icons and assets -->
+    <link href="/fontawesome-free-6.2.1-web/css/v5-font-face.css" rel="stylesheet" />
+    <!-- update the all.css file -->
+    <link href="/fontawesome-free-6.2.1-web/css/all.css" rel="stylesheet">
+    <!-- update any styles .js files you have linked -->
+    <script defer src="/fontawesome-free-6.2.1-web/js/solid.js"></script>
+    <script defer src="/fontawesome-free-6.2.1-web/js/brands.js"></script>
+    <!-- update the core fontawesome.js file -->
+    <script defer src="/fontawesome-free-6.2.1-web/js/fontawesome.js"></script>
+    <!-- update the all.js file -->
+    <script defer src="/fontawesome-free-6.2.1-web/js/all.js"></script>
     <script src="./js/sideBarmenuiconToggle.js"></script>
+
 </head>
 
 <body>
