5d773308386cbdcfb2ec3d26fb61d09d360377b6
DESKTOP-A4FQCPF\sangg <sanggusbab@gmail.com>
2023-02-05 19:26:35 +0900

    contact us workingd

--- include/contactusIndex/contentsWrapper.php
+++ include/contactusIndex/contentsWrapper.php
@@ -1,8 +1,8 @@
-<div style="width:900px; height:400px; margin:auto; padding: 50px; background-color:#888;">
-    <div style="width:400px; height:400px; background-color: #444; float: left;">
-        <p>hello</p>
+<div style="width:900px; height:600px; margin:auto; padding: 50px; background-color:#888;">
+    <div style="width:400px; height:600px; background-color: #444; float: left; overflow: hidden;">
+        <img src="" alt="" style="width:400px; height:600px;">
     </div>
     <div style="width:400px; height:400px; background-color: #444; float: right;">
         <p>hello</p>
     </div>
-</div >
+</div>
