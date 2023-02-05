e69cc19811298acc6100a8e9555afc472dbd78e5
DESKTOP-A4FQCPF\sangg <sanggusbab@gmail.com>
2023-02-05 21:44:13 +0900

    contactus updated

--- include/contactusIndex/contentsWrapper.php
+++ include/contactusIndex/contentsWrapper.php
@@ -12,20 +12,20 @@
     </div>
     <div
         style="position: absolute; right:35px; width:630px; height:630px; background-color: #fff; box-shadow: 20px 20px 20px rgba(0, 0, 0, 0.7) ;float: right;border-radius: 15px; z-index:2">
-        <div class="rows" style="width: 80%; margin:20px 10% 10px;">
-            <p style="color: #444;">
+        <div class="rows" style="width: 80%; margin:20px 10% 10px; text-align:left;">
+            <p style="color: #666;">
                 이름(Name)
             </p>
         </div>
         <div class="rows">이름을 입력해주세요.</div>
-        <div class="rows" style="width: 80%; margin:20px 10% 10px;">
-            <p style="color: #444;">
+        <div class="rows" style="width: 80%; margin:20px 10% 10px; text-align:left;">
+            <p style="color: #666;">
                 이메일(Email)
             </p>
         </div>
         <div class="rows">이메일을 입력해주세요.</div>
-        <div class="rows" style="width: 80%; margin:20px 10% 10px;">
-            <p style="color: #444;">
+        <div class="rows" style="width: 80%; margin:20px 10% 10px; text-align:left;">
+            <p style="color: #666;">
                 내용(Message)
             </p>
         </div>
