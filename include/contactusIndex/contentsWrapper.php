aab44bf155bce42291a8fba1f530542d4410db47
DESKTOP-A4FQCPF\sangg <sanggusbab@gmail.com>
2023-02-05 21:26:30 +0900

    image updated

--- include/contactusIndex/contentsWrapper.php
+++ include/contactusIndex/contentsWrapper.php
@@ -6,12 +6,12 @@
             style="width:500px; height:700px;">
     </div>
     <div
-        style="position: absolute; right: 0px; top:0px; width:500px; height:700px; background-color: #444; overflow: hidden;border-top-right-radius: 15px; border-bottom-right-radius: 15px;">
+        style="position: absolute; right: 0px; top:0px; width:500px; height:700px; background-color: #444; overflow: hidden;border-top-right-radius: 15px; border-bottom-right-radius: 15px; z-index:1">
         <img src="./images/resource/contactusIndex/contactusLeftImage.jpg" alt="contactusBackground"
             style="width:500px; height:700px;">
     </div>
     <div
-        style="width:430px; height:630px; background-color: #fff; box-shadow: 0 0 20px rgba(255, 255, 255, 0.9) ;float: right;border-radius: 15px;">
+        style="width:430px; height:630px; background-color: #fff; box-shadow: 0 0 20px rgba(255, 255, 255, 0.9) ;float: right;border-radius: 15px; z-index:2">
         <div class="rows">이름(Name)</div>
         <div class="rows">이름을 입력해주세요.</div>
         <div class="rows">이메일(Email)</div>
