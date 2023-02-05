efa3ca76a9a6c0d85f2016efb1aea1cef61ef730
DESKTOP-A4FQCPF\sangg <sanggusbab@gmail.com>
2023-02-05 23:01:38 +0900

    contactus updated

--- include/contactusIndex/contentsWrapper.php
+++ include/contactusIndex/contentsWrapper.php
@@ -70,7 +70,7 @@
         </div>
         <div class="rows">
             <button type="button" onclick="location.href = './registerIndex.php'"
-                style="width:280px; height:40px; margin-top:15px; font-size:18px; background-color: rgba(139, 28, 33, 1); color: white; box-shadow: 0 0 18px rgba(139, 28, 33, 0.4); font-weight: bold; border-radius: 16px; border: none; transition: 0.35s;">
+                style="width:280px; height:40px; font-size:18px; background-color: rgba(139, 28, 33, 1); color: white; box-shadow: 0 0 18px rgba(139, 28, 33, 0.4); font-weight: bold; border-radius: 16px; border: none; transition: 0.35s;">
                 <p style="font-size:18px;margin:auto;">제 출 하 기</p>
             </button>
         </div>
