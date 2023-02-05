1944e7a7fa9318c52360e5138a7eed3bb4d55c34
DESKTOP-A4FQCPF\sangg <sanggusbab@gmail.com>
2023-02-05 22:09:20 +0900

    contactus updated

--- include/contactusIndex/contentsWrapper.php
+++ include/contactusIndex/contentsWrapper.php
@@ -13,8 +13,8 @@
     <div
         style="position: absolute; right:35px; width:570px; height:630px; padding: 0 30px;background-color: #fff; box-shadow: 20px 20px 20px rgba(0, 0, 0, 0.7) ;float: right;border-radius: 15px; z-index:2">
         <div class="rows" style="width: 100%; margin:20px 0 10px; text-align:center; float: left;">
-            <p style="color: #666; font-size:32px;">
-                <b>Contact Us</b>
+            <p>
+                <b style="color: #444; font-size:48px;">Contact Us</b>
             </p>
         </div>
         <div class="rows" style="width: 50%; text-align:left; float: left; margin-bottom:10px;">
