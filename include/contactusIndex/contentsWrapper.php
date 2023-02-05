011b46ce1aca3a5f64f1f3ba4302af029b1485ce
DESKTOP-A4FQCPF\sangg <sanggusbab@gmail.com>
2023-02-05 23:13:26 +0900

    image updated

--- include/contactusIndex/contentsWrapper.php
+++ include/contactusIndex/contentsWrapper.php
@@ -46,7 +46,7 @@
                     <input type="text" name="email" placeholder="010"
                         style="width:120px;height:25px;font-size:14px;text-align:center; float:left;">
                 </div>
-                <div style="width: 30px; color: #666; font-size:16px; float:left;">
+                <div style="width: 30px; height:auto; color: #666; font-size:16px; float:left;">
                     <p style="text-align: center;margin:auto;">-</p>
                 </div>
 
@@ -54,7 +54,7 @@
                     <input type="text" name="email" placeholder="1234"
                         style="width:120px;height:25px;font-size:14px;text-align:center; float:left;">
                 </div>
-                <div style="width: 30px; color: #666; font-size:16px; float:left;">
+                <div style="width: 30px; height:auto; color: #666; font-size:16px; float:left;">
                     <p style="text-align: center;margin:auto;">-</p>
                 </div>
 
