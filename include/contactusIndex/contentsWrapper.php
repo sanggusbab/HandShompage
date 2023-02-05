0ee60d640e894e6169b8c9aa1c866a93393cbab5
DESKTOP-A4FQCPF\sangg <sanggusbab@gmail.com>
2023-02-05 20:27:19 +0900

    contactus designing

--- include/contactusIndex/contentsWrapper.php
+++ include/contactusIndex/contentsWrapper.php
@@ -1,8 +1,8 @@
-<div style="position:relative; width:930px; height:730px; margin:auto; padding: 35px; background-color:rgba(255, 255, 255, 0.5); box-shadow: 0 0 40px rgba(255, 255, 255, 0.5);">
-    <div style="position: absolute; left: 0px; top:0px; width:500px; height:800px; background-color: #444; float: left; overflow: hidden;">
+<div style="position:relative; width:930px; height:630px; margin:auto; padding: 35px; background-color:rgba(255, 255, 255, 0.5); box-shadow: 0 0 40px rgba(255, 255, 255, 0.5);">
+    <div style="position: absolute; left: 0px; top:0px; width:500px; height:700px; background-color: #444; float: left; overflow: hidden;">
         <img src="./images/resource/contactusIndex/contactusBackground.jpg" alt="contactusBackground" style="width:500px; height:700px;">
     </div>
-    <div style="width:430px; height:730px; background-color: #444; float: right;">
+    <div style="width:430px; height:630px; background-color: #444; float: right;">
         <p>hello</p>
     </div>
 </div>
