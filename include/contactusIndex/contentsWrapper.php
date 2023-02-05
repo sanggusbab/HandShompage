eb523a5913ae4c2c25a3152d2cd749e507eab8e4
DESKTOP-A4FQCPF\sangg <sanggusbab@gmail.com>
2023-02-05 18:39:25 +0900

    aboutIndex margin updated

--- include/contactusIndex/contentsWrapper.php
+++ include/contactusIndex/contentsWrapper.php
@@ -1,6 +1,6 @@
-<div style="width:400px; height:400px; background-color: #444; margin:auto 10px;">
+<div style="width:400px; height:400px; background-color: #444; margin:auto;">
     <p>hello</p>
 </div>
-<div style="width:400px; height:400px; background-color: #444; margin:auto 10px;">
+<div style="width:400px; height:400px; background-color: #444; margin:auto;">
     <p>hello</p>
 </div>
