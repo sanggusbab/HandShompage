7aea7dac2a8c1df6d0ded0a5bd694dce16d9251e
DESKTOP-A4FQCPF\sangg <sanggusbab@gmail.com>
2023-02-05 22:01:05 +0900

    contactus updated

--- include/contactusIndex/contentsWrapper.php
+++ include/contactusIndex/contentsWrapper.php
@@ -1,12 +1,12 @@
 <div
-    style="position:relative; width:1130px; height:630px; margin:auto; padding: 35px; background-color:rgba(255, 255, 255, 0.8); box-shadow: 0 0 50px rgba(0, 0, 0, 0.8);border-radius: 15px;">
+    style="position:relative; width:1130px; height:630px; margin:auto; padding: 35px; box-shadow: 0 0 50px rgba(0, 0, 0, 0.8);border-radius: 15px;">
     <div
         style="position: absolute; left: 0px; top:0px; width:500px; height:700px; background-color: #444; float: left; overflow: hidden;border-top-left-radius: 15px; border-bottom-left-radius: 15px;">
         <img src="./images/resource/contactusIndex/contactusLeftImage.jpg" alt="contactusBackground"
             style="width:500px; height:700px;">
     </div>
     <div
-        style="position: absolute; right: 0px; top:0px; width:700px; height:700px; background-color: #444; overflow: hidden;border-top-right-radius: 15px; border-bottom-right-radius: 15px; z-index:1; opacity:0.7">
+        style="position: absolute; right: 0px; top:0px; width:700px; height:700px; overflow: hidden;border-top-right-radius: 15px; border-bottom-right-radius: 15px; z-index:1; opacity:0.7">
         <img src="./images/resource/contactusIndex/contactusLeftImage2.jpg" alt="contactusBackground"
             style="width:700px; height:700px;">
     </div>
