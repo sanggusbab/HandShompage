aa22c04820d7f5cd8088cafe012c66b32d67f86b
DESKTOP-A4FQCPF\sangg <sanggusbab@gmail.com>
2023-02-05 20:31:22 +0900

    contactus designing

--- include/contactusIndex/contentsWrapper.php
+++ include/contactusIndex/contentsWrapper.php
@@ -1,8 +1,8 @@
-<div style="position:relative; width:930px; height:630px; margin:auto; padding: 35px; background-color:rgba(255, 255, 255, 0.5); box-shadow: 0 0 40px rgba(255, 255, 255, 0.5);border-radius: 10px;">
-    <div style="position: absolute; left: 0px; top:0px; width:500px; height:700px; background-color: #444; float: left; overflow: hidden;border-radius: 10px;">
-        <img src="./images/resource/contactusIndex/contactusBackground.jpg" alt="contactusBackground" style="width:500px; height:700px;border-radius: 10px;">
+<div style="position:relative; width:930px; height:630px; margin:auto; padding: 35px; background-color:rgba(255, 255, 255, 0.5); box-shadow: 0 0 40px rgba(255, 255, 255, 0.5);border-radius: 15px;">
+    <div style="position: absolute; left: 0px; top:0px; width:500px; height:700px; background-color: #444; float: left; overflow: hidden;border-top-left-radius: 15px;border-bottom-left-radius: 15px;">
+        <img src="./images/resource/contactusIndex/contactusBackground.jpg" alt="contactusBackground" style="width:500px; height:700px;">
     </div>
-    <div style="width:430px; height:630px; background-color: #444; float: right;">
+    <div style="width:430px; height:630px; background-color: #444; float: right;border-radius: 15px;">
         <p>hello</p>
     </div>
 </div>
