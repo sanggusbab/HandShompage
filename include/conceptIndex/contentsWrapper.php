7604565fc2a693e615bef551d0e012edda20edeb
DESKTOP-A4FQCPF\sangg <sanggusbab@gmail.com>
2023-02-05 15:48:50 +0900

    conceptIndex Pagd uploaded

---
+++ include/conceptIndex/contentsWrapper.php
@@ -0,0 +1,24 @@
+<P>
+    <b style="font-size: 24px;">CONCEPT</b>
+    <br>
+    <br>
+</P>
+<div class="rows" style="text-align: center;">
+    <p><b>언제 시작했나요?</b></p> <br>
+</div>
+<div class="rows" style="text-align: center; height: 300px">
+    <img src="images/etc/handshistorytimelinesample.png" alt="handshistorytimelinesample" class="handshistorytimelinesample">
+</div>
+<div class="rows" style="text-align: center;">
+    <p>
+        1981 핸즈 스터디그룹 조직<br>
+        1985 핸즈 조직<br>
+        1985 전자공학과 전자전시회 주관<br>
+        1990 제 1회 HandS 전시회 개최<br>
+        2016년 HandS 30주년 행사<br>
+        2022 HandS 홈커밍 행사
+    </p>
+</div>
+<div>
+    이상 핸즈의 Concept이었습니다.
+</div>
