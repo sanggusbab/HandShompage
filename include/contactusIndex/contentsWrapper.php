f6e6eb42858c390db33a2d822664cc2dda9703f8
DESKTOP-A4FQCPF\sangg <sanggusbab@gmail.com>
2023-02-05 21:30:41 +0900

    image updated

--- include/contactusIndex/contentsWrapper.php
+++ include/contactusIndex/contentsWrapper.php
@@ -1,5 +1,5 @@
 <div
-    style="position:relative; width:930px; height:630px; margin:auto; padding: 35px; background-color:rgba(255, 255, 255, 0.8); box-shadow: 0 0 50px rgba(0, 0, 0, 0.8);border-radius: 15px;">
+    style="position:relative; width:1130px; height:630px; margin:auto; padding: 35px; background-color:rgba(255, 255, 255, 0.8); box-shadow: 0 0 50px rgba(0, 0, 0, 0.8);border-radius: 15px;">
     <div
         style="position: absolute; left: 0px; top:0px; width:500px; height:700px; background-color: #444; float: left; overflow: hidden;border-top-left-radius: 15px; border-bottom-left-radius: 15px;">
         <img src="./images/resource/contactusIndex/contactusLeftImage.jpg" alt="contactusBackground"
@@ -11,7 +11,7 @@
             style="width:500px; height:700px;">
     </div>
     <div
-        style="position: absolute; right:35px; width:430px; height:630px; background-color: #fff; box-shadow: 0 0 20px rgba(255, 255, 255, 0.9) ;float: right;border-radius: 15px; z-index:2">
+        style="position: absolute; right:35px; width:630px; height:630px; background-color: #fff; box-shadow: 0 0 20px rgba(255, 255, 255, 0.9) ;float: right;border-radius: 15px; z-index:2">
         <div class="rows">이름(Name)</div>
         <div class="rows">이름을 입력해주세요.</div>
         <div class="rows">이메일(Email)</div>
