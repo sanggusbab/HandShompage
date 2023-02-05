c81f1b4b23b27e902eaa03c0af1b055df0ddb425
DESKTOP-A4FQCPF\sangg <sanggusbab@gmail.com>
2023-02-05 15:57:26 +0900

    conceptIndex Pagd uploaded3

--- include/conceptIndex/contentsWrapper.php
+++ include/conceptIndex/contentsWrapper.php
@@ -3,61 +3,69 @@
     <br>
     <br>
 </P>
-<div class="rows" style="text-align: center;">
-    <p><b>프로젝트 | 대회</b></p> <br>
-</div>
-<div class="rows" style="text-align: center; height: 300px">
-    <img src="images/etc/handshistorytimelinesample.png" alt="handshistorytimelinesample" class="handshistorytimelinesample">
-</div>
-<div class="rows" style="text-align: center;">
-    <p>
-        교내외 대회 및 행사에 참여하며, 지식을 활용해볼 경험을 가집니다!<br>
-        전공학회 HandS에서 주최하고 지원하는 대회를 통해, 성취감과 함께 실습 기회를 가질 수 있습니다!<br>
-        <br>
-    </p>
-</div>
-
-<div class="rows" style="text-align: center;">
-    <p><b>세미나 | 스터디</b></p> <br>
-</div>
-<div class="rows" style="text-align: center; height: 300px">
-    <img src="images/etc/handshistorytimelinesample.png" alt="handshistorytimelinesample" class="handshistorytimelinesample">
-</div>
-<div class="rows" style="text-align: center;">
-    <p>
-        신입생들을 위한 기초 세미나가 열리며,<br>
-        부원들 간의 스터디, 심화적인 세미나를 통해 학습 효율, 동기부여에 있어 상호증진효과를 가집니다!<br>
-        <br>
-    </p>
+<div class="rows" >
+    <div style="text-align: center;">
+        <p><b>프로젝트 | 대회</b></p> <br>
+    </div>
+    <div style="text-align: center; height: 300px">
+        <img src="images/etc/handshistorytimelinesample.png" alt="handshistorytimelinesample" class="handshistorytimelinesample">
+    </div>
+    <div style="text-align: center;">
+        <p>
+            교내외 대회 및 행사에 참여하며, 지식을 활용해볼 경험을 가집니다!<br>
+            전공학회 HandS에서 주최하고 지원하는 대회를 통해, 성취감과 함께 실습 기회를 가질 수 있습니다!<br>
+            <br>
+        </p>
+    </div>
 </div>
 
-<div class="rows" style="text-align: center;">
-    <p><b>홈페이지</b></p> <br>
-</div>
-<div class="rows" style="text-align: center; height: 300px">
-    <img src="images/etc/handshistorytimelinesample.png" alt="handshistorytimelinesample" class="handshistorytimelinesample">
-</div>
-<div class="rows" style="text-align: center;">
-    <p>
-        HandS의 주요한 공지사항 전달뿐만 아니라,<br>
-        세미나, 스터디와 같은 교류활동에 자율성과 활동 편의성을 증진시킵니다!<br>
-        <br>
-    </p>
+<div class="rows" >
+    <div style="text-align: center;">
+        <p><b>세미나 | 스터디</b></p> <br>
+    </div>
+    <div style="text-align: center; height: 300px">
+        <img src="images/etc/handshistorytimelinesample.png" alt="handshistorytimelinesample" class="handshistorytimelinesample">
+    </div>
+    <div style="text-align: center;">
+        <p>
+            신입생들을 위한 기초 세미나가 열리며,<br>
+            부원들 간의 스터디, 심화적인 세미나를 통해 학습 효율, 동기부여에 있어 상호증진효과를 가집니다!<br>
+            <br>
+        </p>
+    </div>
 </div>
 
-<div class="rows" style="text-align: center;">
-    <p><b>선후배의 연결성</b></p> <br>
-</div>
-<div class="rows" style="text-align: center; height: 300px">
-    <img src="images/etc/handshistorytimelinesample.png" alt="handshistorytimelinesample" class="handshistorytimelinesample">
-</div>
-<div class="rows" style="text-align: center;">
-    <p>
-        핸즈는 주기적인 행사를 통해 선후배 간의 연결성을 강화하고,<br>
-        이러한 기회를 통해 선배들의 경험을 듣고, 여러 좋은 기회를 가질 수 있습니다!<br>
-        <br>
-    </p>
-</div>
+<div class="rows">
+    <div style="text-align: center;">
+        <p><b>홈페이지</b></p> <br>
+    </div>
+    <div style="text-align: center; height: 300px">
+        <img src="images/etc/handshistorytimelinesample.png" alt="handshistorytimelinesample" class="handshistorytimelinesample">
+    </div>
+    <div style="text-align: center;">
+        <p>
+            HandS의 주요한 공지사항 전달뿐만 아니라,<br>
+            세미나, 스터디와 같은 교류활동에 자율성과 활동 편의성을 증진시킵니다!<br>
+            <br>
+        </p>
+    </div>
+</div >
+
+<div class="rows">
+    <div style="text-align: center;">
+        <p><b>선후배의 연결성</b></p> <br>
+    </div>
+    <div style="text-align: center; height: 300px">
+        <img src="images/etc/handshistorytimelinesample.png" alt="handshistorytimelinesample" class="handshistorytimelinesample">
+    </div>
+    <div style="text-align: center;">
+        <p>
+            핸즈는 주기적인 행사를 통해 선후배 간의 연결성을 강화하고,<br>
+            이러한 기회를 통해 선배들의 경험을 듣고, 여러 좋은 기회를 가질 수 있습니다!<br>
+            <br>
+        </p>
+    </div>
+</div >
 <div>
     이상 핸즈의 Concept이었습니다.
 </div>
