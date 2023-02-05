8060c501533e6f8b2b66366defe19b8464bc7a30
DESKTOP-A4FQCPF\sangg <sanggusbab@gmail.com>
2023-02-06 04:39:41 +0900

    some changed

--- include/sideBarFooter.php
+++ include/sideBarFooter.php
@@ -13,42 +13,42 @@
     <ul class="sideBarFooterLinksLists">
         <li>
             <a href="http://www.naver.com">
-                <i class="fab fa-github" style="color: white;font-size: 35px;text-shadow:0 0 5px white;"></i>
+                <i class="fab fa-github" style="color: white;font-size: 25px;text-shadow:0 0 5px white;"></i>
             </a>
         </li>
         <li>
             <a href="http://www.naver.com">
-                <i class="fab fa-instagram" style="color: white;font-size: 35px;text-shadow:0 0 5px white;"></i>
+                <i class="fab fa-instagram" style="color: white;font-size: 25px;text-shadow:0 0 5px white;"></i>
             </a>
         </li>
         <li>
             <a href="http://www.naver.com">
-                <i class="fab fa-facebook" style="color: white;font-size: 35px;text-shadow:0 0 5px white;"></i>
+                <i class="fab fa-facebook" style="color: white;font-size: 25px;text-shadow:0 0 5px white;"></i>
             </a>
         </li>
         <li>
             <a href="http://www.naver.com">
-                <i class="fab fa-hire-a-helper" style="color: white;font-size: 35px;text-shadow:0 0 5px white;"></i>
+                <i class="fab fa-hire-a-helper" style="color: white;font-size: 25px;text-shadow:0 0 5px white;"></i>
             </a>
         </li>
         <li>
             <a href="http://www.naver.com">
-                <i class="fab fa-docker" style="color: white;font-size: 35px;text-shadow:0 0 5px white;"></i>
+                <i class="fab fa-docker" style="color: white;font-size: 25px;text-shadow:0 0 5px white;"></i>
             </a>
         </li>
         <li>
             <a href="http://www.naver.com">
-                <i class="fab fa-google-drive" style="color: white;font-size: 35px;text-shadow:0 0 5px white;"></i>
+                <i class="fab fa-google-drive" style="color: white;font-size: 25px;text-shadow:0 0 5px white;"></i>
             </a>
         </li>
         <li>
             <a href="http://www.naver.com">
-                <i class="fab fa-twitter" style="color: white;font-size: 35px;text-shadow:0 0 5px white;"></i>
+                <i class="fab fa-twitter" style="color: white;font-size: 25px;text-shadow:0 0 5px white;"></i>
             </a>
         </li>
         <li>
             <a href="http://www.naver.com">
-                <i class="fab fa-korvue" style="color: white;font-size: 35px;text-shadow:0 0 5px white;"></i>
+                <i class="fab fa-korvue" style="color: white;font-size: 25px;text-shadow:0 0 5px white;"></i>
             </a>
         </li>
     </ul>
