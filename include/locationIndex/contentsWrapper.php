6339171dbadd1cf0b5c02eb3415fea59fde35a8a
DESKTOP-A4FQCPF\sangg <sanggusbab@gmail.com>
2023-02-05 17:29:51 +0900

    contents Container updated

--- include/locationIndex/contentsWrapper.php
+++ include/locationIndex/contentsWrapper.php
@@ -1,30 +1,32 @@
-<P>
-    <b style="font-size: 24px;">찾아 오시는 길(Way To Come)</b>
+<div  style="text-align:left;">
+    <P>
+        <b style="font-size: 24px;">찾아 오시는 길(Way To Come)</b>
+        <br>
+        <br>
+    </P>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3161.832511015934!2d127.02408121531202!3d37.58256147979495!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357cbd4279f29c71%3A0xe4e68bbe651c1494!2z6rOg66Ck64yA7ZWZ6rWQIOyLoOqzte2Vmeq0gA!5e0!3m2!1sko!2skr!4v1675509020385!5m2!1sko!2skr" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    
     <br>
     <br>
-</P>
-<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3161.832511015934!2d127.02408121531202!3d37.58256147979495!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357cbd4279f29c71%3A0xe4e68bbe651c1494!2z6rOg66Ck64yA7ZWZ6rWQIOyLoOqzte2Vmeq0gA!5e0!3m2!1sko!2skr!4v1675509020385!5m2!1sko!2skr" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-
-<br>
-<br>
-<br>
-<b style="font-size: 18px;">상세 정보</b>
-<br>
-<br>
-<p style="float: left; display: table;">
-    <img src="./images/resource/mapPinImage.jpg" alt="mapPinImage" style="width: 20px; display:table-cell; vertical-align:middle; opacity:0.7;">
-    <span style="font-size: 13px; color: #444; display:table-cell; vertical-align:middle;">
-        &nbsp 서울특별시 성북구 안암로 145 신공학관 109호
-    </span>
-<p style="font-size: 13px; color: #444; display:table-cell; vertical-align:middle;">
-    Room 109, New Engineering Building, 145, Anam-ro, Seongbuk-gu, Seoul, Republic of Korea
-</p>
-</p>
-<p style="font-size: 13px; color: #444;">
-    지번 : 안암동 5가 126-16 신공학관 109호
-</p>
-<br>
-<p>
-    <span>궁금하신 내용이 있으시다면? </span>
-    <span><a href="./contactusIndex.php" style="font-size: 11px;">문의하기(CONTACT US)클릭</a></span>
-</p>
+    <br>
+    <b style="font-size: 18px;">상세 정보</b>
+    <br>
+    <br>
+    <p style="float: left; display: table;">
+        <img src="./images/resource/mapPinImage.jpg" alt="mapPinImage" style="width: 20px; display:table-cell; vertical-align:middle; opacity:0.7;">
+        <span style="font-size: 13px; color: #444; display:table-cell; vertical-align:middle;">
+            &nbsp 서울특별시 성북구 안암로 145 신공학관 109호
+        </span>
+    <p style="font-size: 13px; color: #444; display:table-cell; vertical-align:middle;">
+        Room 109, New Engineering Building, 145, Anam-ro, Seongbuk-gu, Seoul, Republic of Korea
+    </p>
+    </p>
+    <p style="font-size: 13px; color: #444;">
+        지번 : 안암동 5가 126-16 신공학관 109호
+    </p>
+    <br>
+    <p>
+        <span>궁금하신 내용이 있으시다면? </span>
+        <span><a href="./contactusIndex.php" style="font-size: 11px;">문의하기(CONTACT US)클릭</a></span>
+    </p>
+</div>
