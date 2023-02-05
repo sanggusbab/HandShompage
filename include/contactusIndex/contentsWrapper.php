3960532c49184fd832273cbf371702d65d7a74de
DESKTOP-A4FQCPF\sangg <sanggusbab@gmail.com>
2023-02-05 22:08:41 +0900

    contactus updated

--- include/contactusIndex/contentsWrapper.php
+++ include/contactusIndex/contentsWrapper.php
@@ -17,13 +17,13 @@
                 <b>Contact Us</b>
             </p>
         </div>
-        <div class="rows" style="width: 50%; text-align:left; float: left;">
+        <div class="rows" style="width: 50%; text-align:left; float: left; margin-bottom:10px;">
             <p style="color: #666; font-size:16px;">
                 이름(First Name)
             </p>
             <input type="text">이름을 입력해주세요.
         </div>
-        <div class="rows" style="width: 50%; text-align:left; float: left;">
+        <div class="rows" style="width: 50%; text-align:left; float: left; margin-bottom:10px;">
             <p style="color: #666;">
                 성(Last Name)
             </p>
