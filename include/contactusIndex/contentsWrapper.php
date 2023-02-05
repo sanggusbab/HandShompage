a76e7eef31af2f893ee5171a4c4a6f6cba29c300
DESKTOP-A4FQCPF\sangg <sanggusbab@gmail.com>
2023-02-05 21:25:34 +0900

    image updated

--- include/contactusIndex/contentsWrapper.php
+++ include/contactusIndex/contentsWrapper.php
@@ -6,19 +6,17 @@
             style="width:500px; height:700px;">
     </div>
     <div
+        style="position: absolute; right: 0px; top:0px; width:500px; height:700px; background-color: #444; overflow: hidden;border-top-right-radius: 15px; border-bottom-right-radius: 15px;">
+        <img src="./images/resource/contactusIndex/contactusLeftImage.jpg" alt="contactusBackground"
+            style="width:500px; height:700px;">
+    </div>
+    <div
         style="width:430px; height:630px; background-color: #fff; box-shadow: 0 0 20px rgba(255, 255, 255, 0.9) ;float: right;border-radius: 15px;">
-        <div class="relative">
-            <div
-                style="position: absolute; right: 0px; top:0px; width:500px; height:700px; background-color: #444; float: right; overflow: hidden;border-top-right-radius: 15px; border-bottom-right-radius: 15px;">
-                <img src="./images/resource/contactusIndex/contactusLeftImage.jpg" alt="contactusBackground"
-                    style="width:500px; height:700px;">
-            </div>
-            <div class="rows">이름(Name)</div>
-            <div class="rows">이름을 입력해주세요.</div>
-            <div class="rows">이메일(Email)</div>
-            <div class="rows">이메일을 입력해주세요.</div>
-            <div class="rows">내용(Message)</div>
-            <div class="rows">내용을 입력해주세요.</div>
-        </div>
+        <div class="rows">이름(Name)</div>
+        <div class="rows">이름을 입력해주세요.</div>
+        <div class="rows">이메일(Email)</div>
+        <div class="rows">이메일을 입력해주세요.</div>
+        <div class="rows">내용(Message)</div>
+        <div class="rows">내용을 입력해주세요.</div>
     </div>
 </div>
