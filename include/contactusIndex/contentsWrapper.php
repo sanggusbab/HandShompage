439d314178b29f0c5b11386a0aef4191ffde390b
DESKTOP-A4FQCPF\sangg <sanggusbab@gmail.com>
2023-02-08 00:13:11 +0900

    some errors corrected

--- include/contactusIndex/contentsWrapper.php
+++ include/contactusIndex/contentsWrapper.php
@@ -27,7 +27,7 @@
         <div class="rows" style="width: 50%; text-align:left; float: left; margin-bottom:20px;">
             <p style="color: #666; font-size:12px; margin-bottom:6px;">
                 &nbsp 성(Last Name)
-            </p>fab fa-airbnb
+            </p>
             <input type="text" name="lastName" placeholder="성을 입력해주세요." style="width:200px;height:26px;font-size:14px;">
         </div>
         <div class="rows" style="width: 100%; text-align:left; float: left; margin-bottom:20px;">
--- include/sideBarFooter.php
+++ include/sideBarFooter.php
@@ -12,7 +12,7 @@
 <div class="sideBarFooterLinks">
     <ul class="sideBarFooterLinksLists">
         <li>
-            <a href="http://www.naver.com">
+            <a href="https://github.com/HandSKoreaUniv">
                 <i class="fab fa-github" style="color: white;font-size: 25px;text-shadow:0 0 5px white;"></i>
             </a>
         </li>
@@ -22,12 +22,12 @@
             </a>
         </li>
         <li>
-            <a href="http://www.naver.com">
+            <a href="https://www.facebook.com/groups/koreahands">
                 <i class="fab fa-facebook" style="color: white;font-size: 25px;text-shadow:0 0 5px white;"></i>
             </a>
         </li>
         <li>
-            <a href="http://www.naver.com">
+            <a href="https://hardwareand.software/">
                 <i class="fab fa-hire-a-helper" style="color: white;font-size: 25px;text-shadow:0 0 5px white;"></i>
             </a>
         </li>
@@ -47,7 +47,7 @@
             </a>
         </li>
         <li>
-            <a href="http://www.naver.com">
+            <a href="https://portal.korea.ac.kr/">
                 <i class="fab fa-korvue" style="color: white;font-size: 25px;text-shadow:0 0 5px white;"></i>
             </a>
         </li>
