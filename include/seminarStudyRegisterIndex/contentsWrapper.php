76aa55d73ac9522b53c486605cb744fe43c1492f
DESKTOP-A4FQCPF\sangg <sanggusbab@gmail.com>
2023-02-15 06:48:17 +0900

    homepage templete updated

--- include/seminarStudyRegisterIndex/contentsWrapper.php
+++ include/seminarStudyRegisterIndex/contentsWrapper.php
@@ -2,15 +2,20 @@
 	저는 스터디를 등록하는 게시판입니다.
 </div>
 <div data-editor="ClassicEditor" data-collaboration="false" data-revision-history="false" style="margin-top:50px;">
-	<div class="centered">
-		<div class="row row-editor">
-			<div class="editor-container" style="width: 1158px;">
-				<div class="editor">
+	<form method='post' action=''>
+		<div class="centered">
+			<div class="row row-editor">
+				<div class="editor-container" style="width: 1158px;">
+					<div class="editor">
 
+					</div>
 				</div>
 			</div>
 		</div>
-	</div>
+		<div class="d-flex justify-content-center">
+			<input type="submit" name="submit" value="Submit" class="btn btn-success">
+		</div>
+	</form>
 </div>
 <script src="./js/ckeditor.js"></script>
 <script>
@@ -121,3 +126,89 @@
 			console.error(error);
 		});
 </script>
+
+<!-- $ftp_server = FTP_IPADDRESS;
+$ftp_port = 21;
+$ftp_user_name = FTP_LOGIN_ID;
+$ftp_user_pass = FTP_LOGIN_PASSWORD;
+$ftp_send_file = FTP_FILE_PATH;
+$ftp_remote_file = FILE_NAME;
+
+// FTP서버 접속
+$conn_id = ftp_connect($ftp_server, $ftp_port);
+
+// FTP서버 접속 실패한 경우
+if($conn_id == false){
+print_r("[IP:".$ftp_server.":".$ftp_port ."] FTP 서버 접속 실패");
+}
+
+// FTP서버 로그인
+$login_result = ftp_login($conn_id, $ftp_user_name, $ftp_user_pass);
+
+// 로그인 실패한 경우
+if($login_result == false){
+print_r("[IP:".$ftp_server.":".$ftp_port ."], [USER:".$ftp_user_name."] 로그인 실패");
+}
+
+// 패시브 모드 설정
+ftp_pasv($conn_id, true);
+
+// FTP 서버에 파일 전송
+if (ftp_put($conn_id, $ftp_remote_file, $ftp_send_file, FTP_BINARY)) {
+print_r("파일 전송 (ftp) -> UPLOAD 성공");
+} else {
+print_r("파일 전송 (ftp:".$conn_id.",".$ftp_remote_file.",".$ftp_send_file.") -> UPLOAD 실패");
+}
+
+// FTP 서버와 연결 끊음
+ftp_close($conn_id); 
+
+
+
+
+$ftp_server = FTP_IPADDRESS;
+$ftp_port = 21;
+$ftp_user_name = FTP_LOGIN_ID;
+$ftp_user_pass = FTP_LOGIN_PASSWORD;
+$ftp_send_file = FTP_FILE_PATH;
+$ftp_remote_file = FILE_NAME;
+
+try{
+
+    // FTP서버 접속
+    $conn_id = ftp_connect($ftp_server, $ftp_port);
+
+    // FTP서버 접속 실패한 경우 Exception 처리
+    if($conn_id == false){
+        throw new Exception("[IP:".$ftp_server.":".$ftp_port ."] FTP 서버 접속 실패");
+    }
+
+    // FTP서버 로그인
+    $login_result = ftp_login($conn_id, $ftp_user_name, $ftp_user_pass);
+
+    // FTP서버 로그인 실패한 경우 Exception 처리
+    if($login_result == false){
+        throw new Exception("[IP:".$ftp_server.":".$ftp_port ."], [USER:".$ftp_user_name."] 로그인 실패");
+    }
+
+    // 패시브 모드 설정
+    ftp_pasv($conn_id, true);
+
+    // FTP 서버에 파일 전송
+    if (ftp_put($conn_id, $ftp_remote_file, $ftp_send_file, FTP_BINARY)) {
+        print_r("파일 전송 (ftp) -> UPLOAD 성공");
+    } else {
+    // FTP서버 파일 전송 실패한 경우 Exception 처리
+        throw new Exception("파일 전송 (ftp:".$conn_id.",".$ftp_remote_file.",".$ftp_send_file.") -> UPLOAD 실패");
+    }
+
+    // FTP 서버와 연결 끊음
+    ftp_close($conn_id);
+
+} catch(Exception $e) {
+
+    // FTP 서버와 연결 끊음
+    ftp_close($conn_id);
+    print_r($e->getMessage());
+}
+-->
